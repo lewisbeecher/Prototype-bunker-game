38114ec4114ac42f40c0c24a0eaaaeb21037956f
lewisbeecher <145660844+lewisbeecher@users.noreply.github.com>
2023-11-16 15:36:49 +0000

    hjlh

--- Prototype bunker game/Assets/Scripts/Camera_Movement.cs
+++ Prototype bunker game/Assets/Scripts/Camera_Movement.cs
@@ -4,28 +4,6 @@ using UnityEngine;
 
 public class Camera_Movement : MonoBehaviour
 {
-    public float leftLimit, rightLimit, topLimit, bottomLimit;
-    public float moveSpeed;
-
-    Vector2 moveDirection;
-    Rigidbody2D rb2D;
-
-    void Start()
-    {
-        rb2D = GetComponent<Rigidbody2D>();
-    }
-
-    void FixedUpdate()
-    {
-        Move();
-    }
-
-    void Update()
-    {
-        transform.position = new Vector2
-        {
-            Mathf.Clamp(transform.position.x, leftLimit, rightLimit);
-            Mathf.Clamp(transform.position.y bottomLimit, topLimit)
-        }
-    }
+  
+    
 }
