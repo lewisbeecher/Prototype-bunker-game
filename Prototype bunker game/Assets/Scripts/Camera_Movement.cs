1917347bd64f6ea9c134147fad0035912b19c65c
lewisbeecher <145660844+lewisbeecher@users.noreply.github.com>
2023-11-17 14:22:16 +0000

    stuff

--- Prototype bunker game/Assets/Scripts/Camera_Movement.cs
+++ Prototype bunker game/Assets/Scripts/Camera_Movement.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class Camera_Movement : MonoBehaviour
 {
+    public float leftLimit, rightLimit, topLimit, bottomLimit;
+    public float moveSpeed;
+
   
+
+    Vector2 moveDirection;
+    Rigidbody2D rb2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit));
+    }
+        void Move()
+    {
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.y = Input.GetAxisRaw("Vertical");
+
+        rb2D.MovePosition(rb2D.position + moveDirection * moveSpeed * Time.deltaTime);
+    }
     
 }
